808693c96809aebb3c6ce5cae45e4beb9a3b1b63
Thomas <tomasko.mojzis@gmail.com>
2022-11-28 15:00:49 +0100

    font_fix_1

--- js/script.js
+++ js/script.js
@@ -4,3 +4,14 @@ const navbarLinks = document.getElementsByClassName("menu-list")[0]
 toggleButton.addEventListener("click", () => {
     navbarLinks.classList.toggle("active")
 })
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add("show");
+        } else {
+            entry.target.classList.remove("show");
+        }
+    });
+});
